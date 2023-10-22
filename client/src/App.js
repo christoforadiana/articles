22d58b5c46e273212f91fbc3f505547d8ddd1ab3
christoforadiana <christoforadianayuliawati@gmail.com>
2023-10-22 23:09:59 +0700

    project init

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/style.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Redux Toolkit</h1>
+    </div>
+  );
+}
+
+export default App;
